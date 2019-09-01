81ac68489c8490a6ee6a12510609e795ffa40d00
Neeleshwari <neeleshwari@gmail.com>
2019-09-01 11:13:41 +0800

    Create PREP_FIRSTRECUR.cpp

---
+++ PREP_FIRSTRECUR.cpp
@@ -0,0 +1,28 @@
+// PREP_FIRSTRECUR.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	string str = "GeeksandGeeks",visitedstr;
+	int pos;
+
+	for(int i=0;i<str.size();i++)
+	{
+		pos = visitedstr.find(str.at(i));
+		if ( pos != string::npos )
+		{
+			cout<<"The first visited character is : "<<str.at(i);
+			cout<<"\nOriginal position : "<<pos<<"\nRecurring position : "<<i;
+			break;
+		}
+		visitedstr = visitedstr + str.at(i);
+	}
+	return 0;
+}
+
