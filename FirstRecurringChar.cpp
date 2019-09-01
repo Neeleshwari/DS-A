91c80069b904eebc55631598dde169a00bf2f11b
Neeleshwari <neeleshwari@gmail.com>
2019-09-01 22:46:25 +0800

    Create FirstRecurringChar.cpp

---
+++ FirstRecurringChar.cpp
@@ -0,0 +1,36 @@
+// PREP_FIRSTRECUR.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+#include<hash_set>
+
+using namespace stdext;
+using namespace std;
+
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	string str = "ABCKFCDBA";
+	int key;
+
+	hash_set<char> visitedmap;
+	hash_set<char>::iterator hset_itr;
+	
+
+	for(int i=0;i<str.size();i++)
+	{
+		hset_itr = visitedmap.find(str.at(i));
+		if ( hset_itr != visitedmap.end() )
+		{
+			cout<<"The first visited character is : "<<str.at(i);
+			cout<<"\nRecurring position : "<<i;
+			break;
+		}
+		visitedmap.insert(str.at(i));
+	}
+	return 0;
+}
